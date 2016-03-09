78bee78dd8b348fca147aea8a5800498efd34618
Reisz <leinad.ffafp@gmail.com>
2016-03-09 11:12:50 +0100

    Finished design for Event.

--- src/util/Event.lua
+++ src/util/Event.lua
@@ -1,110 +1,140 @@
-local Set = require "util.set"
+local class = require "util.middleclass"
+local remove = table.remove
 
-local Event = {}
+local Event = class("Event")
+
+
 
 local objects = setmetatable({}, {__mode = "k"})
-Event.objects = objects
-
--- ObjectData: contains map<String(eventName), HandlerMap>
-local function createObjectData(object)
-  local data = {}; objects[object] = data; return data
-end
+Event.static.objects = objects
 
 local function getObjectData(object)
-  return objects[object] or createObjectData(object)
-end; Event.getObjectData = getObjectData
+  return objects[object]
+end; Event.static.getObjectData = getObjectData
 
--- HandlerMap: contains map<String(selector), HandlerSet>
-local function createHandlerMap(objectData, event)
-  local map = {}; objectData[event] = map; return map
-end
+local function newObjectData(object)
+  local data = {}; objects[object] = data; return data
+end; Event.static.newObjectData = newObjectData
 
-local function getHandlerMap(object, event)
+local function getHandlerList(object, event)
   local objectData = getObjectData(object)
-  return objectData[event] or createHandlerMap(objectData, event)
-end; Event.getHandlerMap = getHandlerMap
+  return objectData and objectData[event]
+end Event.static.getHandlerList = getHandlerList
 
--- HandlerSet: contains set<Handler>
-local function createHandlerSet(handlerMap, selector)
-  local set = Set(); handlerMap[selector] = set; return set
-end
+local function newHandlerList(object, event)
+  local objectData = getObjectData(object) or newObjectData(object)
+  local list = {}; objectData[event] = list; return list
+end Event.static.newHandlerList = newHandlerList
 
-local function getHandlerSet(object, event, selector)
-  local handlerMap = getHandlerMap(object, event)
-  return handlerMap[selector] or createHandlerSet(handlerMap, selector)
-end; Event.getHandlerSet = getHandlerSet
+local function newHandler(fun, data)
+  return {fun, data}
+end; Event.static.newHandler = newHandler
 
+local function addHandler(object, event, handler)
+  local handlerList = getHandlerList(object, event) or newHandlerList(object, event)
+  local index = #handlerList + 1; handlerList[index] = handler; return index
+end; Event.static.addHandler = addHandler
 
-local function addHandler(object, event, selector, data, handler)
-  getHandlerSet(object, event, selector):insert{handler, data}
-end; Event.addHandler = addHandler
+local function removeHandler(object, event, index)
+  local handlerList = getHandlerList(object, event)
+  if handlerList then remove(handlerList, index) end
+end; Event.static.removeHandler = removeHandler
 
-local function filter(v, handler) return v[1] == handler end
-local function removeHandler(object, event, selector, handler)
-  if not handler then -- removeEveryHandler for this event/selector
-    getHandlerMap(object, event)[selector] = nil
-  else -- remove specific types of handlers from this event/selector
-    getHandlerSet(object, event, selector):removeFilter(filter, handler)
-  end
-end; Event.removeHandler = removeHandler
-
--- TODO
--- luacheck: no unused
-local function triggerHandler(object, event, selector, eventObject, ...)
-  getHandlerSet(object, event, selector)
-end
-
-Event.mixin = {}
-local function args_on(p1,p2,p3)
-  if p3 then
-    return p1 or "", p2, p3
-  elseif p2 and type(p1) == "string" then
-    return p1 or "", nil, p2
-  elseif p2 then
-    return "", p1, p2
-  end
-
-  return "", nil, p1
-end
-
-local function args_off(p1, p2)
-  if p2 then
-    return p1 or "", p2
-  elseif type(p1) == "string" then
-    return p1 or ""
-  end
-
-  return "", p1
-end
-
--- :on(events:string, [selector:string], [data:table], handler:callable)
-function Event.mixin:on(events, p1, p2, p3)
-  local selector, data, handler = args_on(p1,p2,p3)
-  for event in string.gmatch(events, "[^ ]+") do
-    addHandler(self, event, selector, data, handler)
-  end
-end
-
--- :one(events:string, [selector:string], [data:table], handler:callable)
-function Event.mixin:one(events, p1, p2, p3)
-  local selector, data, handler = args_on(p1,p2,p3)
-  for event in string.gmatch(events, "[^ ]+") do
-    local onceFn; onceFn = function(e, ...)
-      e.data = data; handler(e, ...)
-      removeHandler(self, event, selector, onceFn)
+local function removeByFunction(object, event, fun)
+  local oldHandlerList = getHandlerList(object, event)
+  if oldHandlerList then
+    local handlerList, index = newHandlerList(object, event), 1
+    for i = 1, #oldHandlerList do
+      local oldHd = oldHandlerList[i]
+      if oldHd[1] ~= fun then
+        handlerList[index] = oldHd
+        index = index + 1
+      end
     end
-    addHandler(self, event, selector, nil, onceFn)
+  end
+end; Event.static.removeByFunction = removeByFunction
+
+local function removeAll(object, event)
+  local objectData = getObjectData(object)
+  if objectData then objectData[event] = nil end
+end; Event.removeAll = removeAll
+
+local function triggerEvent(object, event, eventObject, ...)
+  local handlerList = getHandlerList(object, event)
+  for i = 1, # handlerList do
+    local handler = handlerList[i]
+    eventObject.data = handler[2]
+    handler[1](eventObject, ...)
+    if event.stopImmediate then return end
+  end
+
+  if event.stop then return end
+
+  local parent = object.parent
+  if parent then return triggerEvent(parent, eventObject, ...) end
+end; Event.static.triggerEvent = triggerEvent
+
+
+
+Event.static.mixin = {}
+
+local gmatch = string.gmatch
+local function it_events(events) return gmatch(events, "[^ ]+") end
+
+local function dummy() return false end
+Event.static.falseDummy = dummy
+
+-- :on(events:string, [data:table], fun:callable|<false>)
+function Event.static.mixin:on(events, data, fun)
+  if type(fun) == "nil" then fun = data; data = nil end
+  if fun == false then fun = dummy end
+  for event in it_events(events) do
+    addHandler(self, event, newHandler(fun, data))
   end
 end
 
--- :off(events:string, [selector:string], [handler:string])
-function Event.mixin:off(events, p1, p2)
-  local selector, handler = args_off(p1,p2)
-  for event in string.gmatch(events, "[^ ]+") do
-    removeHandler(self, event, selector, handler)
+-- :one(events:string, [data:table], fun:callable|false)
+function Event.static.mixin:one(events,  data, fun)
+  if type(fun) == "nil" then fun = data; data = nil end
+  if fun == false then fun = dummy end
+  for event in it_events(events) do
+    local index, onceFun; onceFun = function(e, ...)
+      e.data = data; fun(e, ...)
+      removeHandler(self, event, index)
+    end
+    index = addHandler(self, event, newHandler(onceFun, nil))
   end
 end
 
-function Event.mixin:trigger(event, ...)
-  triggerHandler(self, event, self.class.name, ...)
+-- :off(events:string, [fun:callable|false])
+function Event.static.mixin:off(events, fun)
+  if type(fun) == "nil" then
+    for event in it_events(events) do removeAll(self, event) end
+  else
+    if fun == false then fun = dummy end
+    for event in it_events(events) do
+      removeByFunction(self, event, fun)
+    end
+  end
 end
+
+-- :tigger(event:string, ...)
+function Event.static.mixin:trigger(event, ...)
+  local eventObject = Event(event, self)
+  triggerEvent(self, event, eventObject, ...)
+end
+
+
+
+function Event:initialize(type, target)
+  self.type, self.target = type, target
+  self.stop, self.stopImmediate = false, false
+end
+
+function Event:stopPropagation() self.stop = true end
+function Event:stopImmediatePropagation() self.stopImmediate = true end
+
+function Event:getTarget() return self.target end
+function Event:getData() return self.data end
+
+return Event
